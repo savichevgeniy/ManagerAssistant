da65c1ab9138fb311a86c65f3ca552b1a520f9f0
Savich Evgeniy <Savich Evgeniy@DESKTOP-VG26JUE>
2020-04-13 00:58:35 +0300

    Add roles in Startup

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace ManagerAssistant
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
