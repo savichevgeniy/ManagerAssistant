84a874f5cf2e00ff98368a32c6b405c286b04175
Savich Evgeniy <Savich Evgeniy@DESKTOP-VG26JUE>
2020-04-13 00:56:59 +0300

    Add roles identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ManagerAssistant.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ManagerAssistant.Areas.Identity.IdentityHostingStartup))]
+namespace ManagerAssistant.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
