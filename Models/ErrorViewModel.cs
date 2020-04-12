64c527e007849dd2143828f1ef5b9edca606c271
Savich Evgeniy <Savich Evgeniy@DESKTOP-VG26JUE>
2020-04-13 00:18:48 +0300

    New Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ManagerAssistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
