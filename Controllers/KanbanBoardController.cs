ed565ccfb5a05c7a2febd1da1facd5fb7d3c6c87
Savich Evgeniy <Savich Evgeniy@DESKTOP-VG26JUE>
2021-01-10 05:13:11 +0300

    Create Kanban board

---
+++ Controllers/KanbanBoardController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ManagerAssistant;
+using ManagerAssistant.Data;
+
+namespace ManagerAssistant.Controllers
+{
+    public class KanbanBoardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KanbanBoardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: KanbanBoard
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Project.ToListAsync());
+        }
+
+        // GET: KanbanBoard/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Project
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // GET: KanbanBoard/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KanbanBoard/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NameProject,Status,Description,DayOnDev,Insurance,TotalDeadline,QuantityPerson,Rent,PaymentForServers,Cost,AverageIncome,TotalIncome,Profit,Tax,CostProject,IdOrder,IdEmployee,IdClient,IdLead")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                project.Id = Guid.NewGuid();
+                _context.Add(project);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+
+        // GET: KanbanBoard/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Project.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        // POST: KanbanBoard/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,NameProject,Status,Description,DayOnDev,Insurance,TotalDeadline,QuantityPerson,Rent,PaymentForServers,Cost,AverageIncome,TotalIncome,Profit,Tax,CostProject,IdOrder,IdEmployee,IdClient,IdLead")] Project project)
+        {
+            if (id != project.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(project);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProjectExists(project.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+
+        // GET: KanbanBoard/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Project
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // POST: KanbanBoard/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var project = await _context.Project.FindAsync(id);
+            _context.Project.Remove(project);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProjectExists(Guid id)
+        {
+            return _context.Project.Any(e => e.Id == id);
+        }
+    }
+}
